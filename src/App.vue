0be5dfa204a83a74b4b3c3edd8244b213b9c11b3
zzb <zzbmnran@gmail.com>
2023-04-16 18:57:06 +0800

    update: add 刷新Token

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,23 @@
-<script setup></script>
+<script setup>
+import { onMounted } from "vue";
+import { useCookies } from "vue3-cookies";
+import { rreloadToken } from "@/api/user";
+
+const { cookies } = useCookies();
+
+function reloadToken() {
+  rreloadToken()
+    .then((res) => {})
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+onMounted(() => {
+  if (!cookies.get("sign")) return;
+  reloadToken();
+});
+</script>
 
 <template>
   <router-view></router-view>
