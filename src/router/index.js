85816fe2bc56264ee956b35fa6a98b1a78c32c16
zzb <zzbmnran@gmail.com>
2023-04-16 18:59:29 +0800

    update:登录路由逻辑

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,46 @@
 import { createWebHistory, createRouter } from "vue-router";
+import { useCookies } from "vue3-cookies";
+
+const { cookies } = useCookies();
 
 const routes = [
   {
     path: "/",
     redirect: "/login",
   },
-
   {
     name: "Login",
     path: "/login",
     component: () => import("@/views/Login.vue"),
+  },
+  {
+    name: "Index",
+    path: "/index",
+    component: () => import("@/views/Layout/Index.vue"),
+  },
+  {
+    name: "Home",
+    path: "/home",
+    component: () => import("@/views/Layout/Home.vue"),
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes: routes,
-  strict: true, 
+  strict: true,
+});
+
+router.beforeEach(function (to, from) {
+  if (to.path !== "/login" && !cookies.get("sign")) {
+    router.push("/login");
+  } else if (to.path === "/login" && cookies.get("sign")) {
+    if (from.path === "/") {
+      router.push("/index");
+    } else {
+      router.push(from.path);
+    }
+  }
 });
 
 export default router;
