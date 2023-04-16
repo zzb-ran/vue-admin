cfb23550a9e3cf360a95e35c002144cfc4af1378
zzb <zzbmnran@gmail.com>
2023-04-16 18:57:50 +0800

    update: add vue3-cookies

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import { createApp } from 'vue'
-import ElementPlus from "element-plus"
-import 'element-plus/dist/index.css'
-import router from "./router"
-import App from './App.vue'
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import router from "./router";
+import vueCookies from "vue3-cookies";
+import App from "./App.vue";
 
-const app = createApp(App)
-app.use(ElementPlus)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(ElementPlus);
+app.use(router);
+app.use(vueCookies);
+app.mount("#app");
